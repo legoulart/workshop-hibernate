ad2ea03c6c835082a34be007895b3f023123dc49
Gabriel Goulart <gabrielfegoulart@gmail.com>
2019-10-03 11:05:10 -0300

    Added exception handler and adjusts into API endpoint

--- src/main/java/com/example/workshop/services/CategoriaService.java
+++ src/main/java/com/example/workshop/services/CategoriaService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 
 import com.example.workshop.domain.Categoria;
 import com.example.workshop.repositories.CategoriaRepository;
+import com.example.workshop.services.exceptions.ObjectNotFoundException;
+
+
 
 @Service
 public class CategoriaService {
@@ -16,6 +19,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
