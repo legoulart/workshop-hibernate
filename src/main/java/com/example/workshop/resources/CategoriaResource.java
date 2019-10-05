545c2960034007d6b7b49afdd293ab2e3c84bd88
Gabriel Goulart <gabrielfegoulart@gmail.com>
2019-10-04 21:46:47 -0300

    Added User's insert via POST

--- src/main/java/com/example/workshop/resources/CategoriaResource.java
+++ src/main/java/com/example/workshop/resources/CategoriaResource.java
@@ -1,14 +1,17 @@
 package com.example.workshop.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.workshop.domain.Categoria;
 import com.example.workshop.services.CategoriaService;
@@ -37,5 +40,13 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id){
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
