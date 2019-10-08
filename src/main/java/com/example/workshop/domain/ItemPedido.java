f926be65e01dbfbbc1e5847e7fe48e2f17e91b2b
Gabriel Goulart <gabrielfegoulart@gmail.com>
2019-10-08 07:34:40 -0300

    Added SubTotal and Total methods to Pedido's class

--- src/main/java/com/example/workshop/domain/ItemPedido.java
+++ src/main/java/com/example/workshop/domain/ItemPedido.java
@@ -8,20 +8,20 @@ import javax.persistence.Entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class ItemPedido implements Serializable{
-	
+public class ItemPedido implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
+
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
-	
+
 	public ItemPedido() {
-		
+
 	}
 
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
@@ -31,17 +31,20 @@ public class ItemPedido implements Serializable{
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
 
-	
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -98,7 +101,5 @@ public class ItemPedido implements Serializable{
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-	
-	
 
 }
